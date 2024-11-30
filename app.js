7c705c222bf117e0a6f4eec0b8ab674692e06810
Drake Scott <ArbiterBibendi@yahoo.com>
2024-11-30 10:08:41 -0600

    create transactions

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express');
-const inventory = require('./routes/inventory.js');
 const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
+const inventory = require('./routes/inventory.js');
+const transactions = require('./routes/transactions.js');
+
 app.use(bodyParser.json());
 app.use('/inventory', inventory);
+app.use('/transactions', transactions);
 
 app.listen(port, () => {
 	console.log(`listening on port ${port}`);
---
+++ routes/transactions.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const db = require('../utils/db');
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+    
+});
